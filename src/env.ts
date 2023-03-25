680b80e4342a59c785a2006298147bb1761fe57f
Jason Sanford <jasonsanford@gmail.com>
2023-03-24 22:13:47 -0400

    database url

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ export const env = envsafe({
   // AWS_SECRET_ACCESS_KEY: str(),
   // AWS_S3_BUCKET: str(),
   // AWS_S3_REGION: str(),
-  BACKUP_DATABASE_URL: str({
+  DATABASE_URL: str({
     desc: "The connection string of the database to backup.",
   }),
   BACKUP_CRON_SCHEDULE: str({
